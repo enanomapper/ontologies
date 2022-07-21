74f666d97a1f709cdc5f4eaa18fa2984190218c5
Javier <javier.millanacosta@maastrichtuniversity.nl>
2022-07-21 15:15:28 +0200

    Added workflow for GH actions

---
+++ scripts/src/build-workflow/make-yaml.py
@@ -0,0 +1,105 @@
+
+#!/usr/local/bin/python
+
+# Variables used throughout the script
+# comment out because aopo takes forever  ontologies = ["aopo","bao","bfo","ccont","cheminf","chmo","efo","fabio","go","hupson","iao","ncit","npo","oae","obcs","obi","pato","sio","uo"]
+ontologies = ["bao","bfo","ccont","cheminf","chmo","efo","fabio","go","iao","ncit","npo","oae","obcs","obi","pato","sio","uo"]
+
+#props = ["bao", "cheminf","npo", "sio"] props npo keeps failing, removed for testing
+props = ["bao", "cheminf", "sio"]
+
+test_location = "test"
+
+# Opens the yaml file
+with open("../../../.github/workflows/slim-ontologies.yml", "a+") as f:
+  # Writes serialization for the workflow dispatch and getting slimmer
+  f.write("""name: workflow slim ontologies
+on:
+  workflow_dispatch:
+
+jobs:
+  get-scripts:
+    runs-on: ubuntu-latest
+    steps:
+    - name: Check out repository
+      uses: actions/checkout@v3
+    # Get slimmer
+    - name: get slimmer
+      run: wget -nc https://github.com/enanomapper/slimmer/releases/download/v1.0.2/slimmer-1.0.2-jar-with-dependencies.jar 
+    # Keep artifact
+    - name: artifact slimmer 
+      uses: actions/upload-artifact@v2
+      with:
+        name: slimmer
+        path: slimmer-1.0.2-jar-with-dependencies.jar
+      
+
+# slim all ontologies
+  """)
+
+  # Writes serialization to slim all ontologies
+  for ontology in ontologies:
+    f.write("""
+# slim {}
+  slim-{}:
+    runs-on: ubuntu-latest
+    needs: get-scripts
+    steps:
+    - name: Check out repository
+      uses: actions/checkout@v3
+    # Authorize running slim script
+    - name: authorize running slim.sh and slim-ncit.sh
+      run: chmod 755 scripts/src/build-workflow/slim.sh scripts/src/build-workflow/slim-ncit.sh
+    - name: get slimmer
+      uses: actions/download-artifact@v2
+      with:
+       name: slimmer 
+    - name: slim {}
+      run: bash scripts/src/build-workflow/slim.sh {}
+    # Keep slimmed ontology as an artifact
+    - name: artifact {}-slim 
+      uses: actions/upload-artifact@v2
+      with:
+        name: {}-slim.owl
+        path: ./external
+
+  # apply props
+      """.format(ontology,ontology,ontology,ontology,ontology,ontology))
+
+  # Apply props
+  for prop in props:
+    f.write("""
+# apply props {}
+  props-{}:
+    runs-on: ubuntu-latest
+    needs: slim-{}
+    steps:
+    - name: Check out repository
+      uses: actions/checkout@v3
+    - name: Apply props {}
+      run: bash scripts/src/build-workflow/props.sh {}
+    # )
+    """.format(prop, prop, prop, prop, prop))
+  # Tests
+  f.write("""
+  ## test
+  #test:
+  #  runs-on: ubuntu-latest
+  #  needs: {}
+  #  steps:
+  #  - name: Check out repository
+  #    uses: actions/checkout@v3
+  #  - name: Run mvn clean test 
+  #    run: |
+  #     cd test 
+  #     mvn clean
+  #     cd ../
+  #     zip –r ../test.zip test
+  #  # Keep tests as artifact
+  #  - name: artifact tests
+  #    uses: actions/upload-artifact@v2
+  #    with:
+  #      name: test
+  #      path: test.zip
+
+  """.format(("[slim-" + ", slim-".join(ontologies) + "]").replace("'","")))
---
+++ scripts/src/build-workflow/slim.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Read the ontology name (first argument) and change to its directory
+export ONTO=$1
+if [ ${ONTO} == "$ncit" ]; then 
+    ./slim-ncit.sh
+    else
+    
+    ls
+    mkdir -p external/${ONTO}
+    cd external/${ONTO}
+   
+   
+    wget https://raw.githubusercontent.com/enanomapper/ontologies/master/config/${ONTO}.props
+    wget  `grep "owl=" ${ONTO}.props | cut -d'=' -f2`
+    wget https://raw.githubusercontent.com/enanomapper/ontologies/master/config/${ONTO}.iris
+    
+    
+    # Run slimmer
+    
+    java -cp ../../slimmer-1.0.2-jar-with-dependencies.jar com.github.enanomapper.Slimmer .
+    
+    # Remove original owl file
+    ontology=$(basename `grep "owl=" ${ONTO}.props | cut -d'=' -f2`)
+    rm -f $ontology
+    
+    # Rename slimmed file to proper name
+    mv *-slim.owl ../${ONTO}-slim.owl
