acd4b7216ff294f4af5148d910b64e8238cfe4ef
jmillanacosta <javier.millan.acosta@gmail.com>
2022-09-27 12:02:06 +0200

    Updated workflow to make a PR upon completion

--- scripts/src/build-workflow/make-yaml.py
+++ scripts/src/build-workflow/make-yaml.py
@@ -1,4 +1,3 @@
-
 #!/usr/local/bin/python
 
 # Variables used throughout the script
@@ -7,6 +6,8 @@ ontologies = ["aopo", "bao","bfo","bto","ccont","cheminf","cito","chebi","chmo",
 props = ["bao", "cheminf","npo", "sio", "ro", "cito"]
 
 test_location = "test"
+
+assignees = "jmillanacosta"
 
 # Opens the yaml file
 with open("../../../.github/workflows/slim-ontologies.yml", "a+") as f:
@@ -17,7 +18,7 @@ on:
   workflow_dispatch:
 
 jobs:
-  get-scripts:
+  build:
     runs-on: ubuntu-latest
     steps:
     - name: Check out repository
@@ -25,86 +26,39 @@ jobs:
     # Get slimmer
     - name: get slimmer
       run: wget -nc https://github.com/enanomapper/slimmer/releases/download/v1.0.2/slimmer-1.0.2-jar-with-dependencies.jar 
-    # Keep artifact
-    - name: artifact slimmer 
-      uses: actions/upload-artifact@v2
-      with:
-        name: slimmer
-        path: slimmer-1.0.2-jar-with-dependencies.jar
-      
-
-# slim all ontologies
   """)
 
   # Writes serialization to slim all ontologies
+  f.write("""
+  # slim all ontologies
+  # Authorize running slim script
+    - name: authorize running slim scripts
+      run: chmod 755 scripts/src/build-workflow/*.sh
+  """)
+  # Apply slims
   for ontology in ontologies:
     f.write("""
-# slim {}
-  slim-{}:
-    runs-on: ubuntu-latest
-    needs: get-scripts
-    steps:
-    - name: Check out repository
-      uses: actions/checkout@v3
-    # Authorize running slim script
-    - name: authorize running slim.sh and slim-ncit.sh
-      run: chmod 755 scripts/src/build-workflow/slim.sh scripts/src/build-workflow/slim-ncit.sh
-    - name: get slimmer
-      uses: actions/download-artifact@v2
-      with:
-       name: slimmer 
-    - name: slim {}
+  # Slim {}
+    - name: slim-{}
       run: bash scripts/src/build-workflow/slim.sh {}
-
-  # apply props
-      """.format(ontology,ontology,ontology,ontology,ontology,ontology))
-
+  """.format(ontology, ontology, ontology))
   # Apply props
   for prop in props:
     f.write("""
-# apply props {}
-  props-{}:
-    runs-on: ubuntu-latest
-    needs: slim-{}
-    steps:
-    - name: Check out repository
-      uses: actions/checkout@v3
+  # apply props {}
     - name: Apply props {}
       run: bash scripts/src/build-workflow/props.sh {}
-    # )
-    """.format(prop, prop, prop, prop, prop))
-  # Tests
+  """.format(prop, prop, prop))
+  
+  # PR
   f.write("""
-  retrieve-owl-artifacts:
-    # Keep slimmed ontology as an artifact
-    runs-on: ubuntu-latest
-    needs: {}
-    steps:
-    - name: owl slims artifact
-      uses: actions/upload-artifact@v2
+  # Create PR
+    - name: Create Pull Request
+      uses: peter-evans/create-pull-request@v3
       with:
-        name: owl-slims
-        path: ./external/*slim.owl""".format(("[slim-" + ", slim-".join(ontologies) + "]").replace("'","")))
-
-
-
-  ## test
-  #test:
-  #  runs-on: ubuntu-latest
-  #  needs: {}
-  #  steps:
-  #  - name: Check out repository
-  #    uses: actions/checkout@v3
-  #  - name: Run mvn clean test 
-  #    run: |
-  #     cd test 
-  #     mvn clean
-  #     cd ../
-  #     zip –r ../test.zip test
-  #  # Keep tests as artifact
-  #  - name: artifact tests
-  #    uses: actions/upload-artifact@v2
-  #    with:
-  #      name: test
-  #      path: test.zip
-  #""".format(("[slim-" + ", slim-".join(ontologies) + "]").replace("'","")))
+        commit-message: Update release files
+        title: 'Update release files'
+        body: |
+          Updates all release files.
+        assignees: {}    
+  """.format(assignees))
